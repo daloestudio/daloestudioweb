8f4236eeaa150de6a753292e3ba5fa4c4cea451e
Daiana Selene Lopez <daloestudio@gmail.com>
2020-12-16 19:51:28 -0300

    Desafio Clase 8 DOM

--- scripts/app.js
+++ scripts/app.js
@@ -28,3 +28,9 @@ function calcularPromos(carrito) {
         carrito.promos.push(promo);
     }
 }
+
+let prodsAEliminar = parseInt(prompt("Ingrese cantidad de productos a eliminar"));
+let products = document.getElementsByClassName("store-item");
+for(let i=0;i<prodsAEliminar && products.length > 0; i++) {
+    products[0].parentNode.removeChild(products[0]);
+}
